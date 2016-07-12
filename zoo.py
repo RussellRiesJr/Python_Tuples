3ce99036edca8945176d9d3ddbfbc49b347d0a35
Russell Ries <riesru@gmail.com>
2016-07-12 10:23:56 -0500

    add for value in tuple method

--- zoo.py
+++ zoo.py
@@ -2,6 +2,8 @@ zoo = ('naked mole rat', 'dog', 'monkey', 'lemur', 'python', 'chinchilla', 'goat
 
 print("Index for lemur: ", zoo.index( 'lemur' ))
 
+[print('There is a {0} in the zoo'.format(animal)) for animal in zoo if animal == 'chinchilla']
+
 
 import code
 code.interact(local=locals())
