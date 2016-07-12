578e460cd72c3ee9d61f6c9a9a1d5cbed706ae18
Russell Ries <riesru@gmail.com>
2016-07-12 10:16:15 -0500

    add index of lemur

---
+++ zoo.py
@@ -0,0 +1,7 @@
+zoo = ('naked mole rat', 'dog', 'monkey', 'lemur', 'python', 'chinchilla', 'goat')
+
+print("Index for lemur: ", zoo.index( 'lemur' ))
+
+
+import code
+code.interact(local=locals())
